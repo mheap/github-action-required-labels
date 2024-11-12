db4753a064c6ab5233912702d8428861b1d24183
Victor Martinez <VictorMartinezRubio@gmail.com>
2024-11-12 10:11:01 +0100

    use the correct event name

--- index.js
+++ index.js
@@ -46,12 +46,12 @@ async function action() {
 
     let issue_number = github.context.issue.number;
 
-    if (github.context.eventName === "merge_queue") {
+    if (github.context.eventName === "merge_group") {
       // Parse out of the ref for merge queue
       // e.g. refs/heads/gh-readonly-queue/main/pr-17-a3c310584587d4b97c2df0cb46fe050cc46a15d6
       const lastPart = github.context.ref.split("/").pop();
       issue_number = lastPart.match(/pr-(\d+)-/)[1];
-      core.info(`merge_queue event detected and issue_number parsed as ${issue_number}`);
+      core.info(`merge_group event detected and issue_number parsed as ${issue_number}`);
     }
 
     const allowedModes = ["exactly", "minimum", "maximum"];
